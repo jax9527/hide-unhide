24bdc7a87cb000625f5420f8daafb70b845840ff
RAY LEE <kavo1720@gmail.com>
2021-03-23 00:39:36 +0800

    demo

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import './App.css';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+          display:'block',
+          isToggleOn:true
+      };
+      this.bindClick=this.handleClick.bind(this);
+    }
+  handleClick=()=>{
+
+        this.setState(
+          prevState =>{
+            return {
+              display: prevState.isToggleOn ? 'none': 'block',
+              isToggleOn : !prevState.isToggleOn
+            }
+          }
+        )
+            
+  }
+  render() {
+    return (
+      <div className="App">
+         <button onClick={this.handleClick}>
+                 {this.state.isToggleOn ? "關閉圖片" : "打開圖片"}
+         </button>
+         <div className="i-test"  
+              style={{display: this.state.display}} 
+         >         
+         <img src={"https://reactjs.org/logo-og.png"}/>
+         </div>               
+      </div>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
