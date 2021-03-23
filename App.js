628bf1d7e3780045d8389df9f7f5a762bd48176d
RAY LEE <kavo1720@gmail.com>
2021-03-23 17:35:56 +0800

    pretty

--- App.js
+++ App.js
@@ -2,35 +2,35 @@ import React, { Component } from 'react';
 import Logo from "./logo.png";
 
 class App extends Component {
-    constructor(props){
-      super(props)
-      this.state={
-          display:'block',
-          isToggleOn:true
-      };
-      this.bindClick=this.handleClick.bind(this);
-    }
-  handleClick=()=>{
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: 'block',
+      isToggleOn: true
+    };
+    this.bindClick = this.handleClick.bind(this);
+  }
+  handleClick = () => {
 
-        this.setState(
-          prevState =>{
-            return {
-              display: prevState.isToggleOn ? 'none': 'block',
-              isToggleOn : !prevState.isToggleOn
-            }
-          }
-        )
-            
+    this.setState(
+      prevState => {
+        return {
+          display: prevState.isToggleOn ? 'none' : 'block',
+          isToggleOn: !prevState.isToggleOn
+        }
+      }
+    )
+
   }
   render() {
     return (
       <div>
-         <button onClick={this.handleClick}>
-                 {this.state.isToggleOn ? "關閉圖片" : "打開圖片"}
-         </button>
-         <div style={{display: this.state.display}} >         
-         <img src={Logo} alt="Logo"/>
-         </div>               
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "關閉圖片" : "打開圖片"}
+        </button>
+        <div style={{ display: this.state.display }} >
+          <img src={Logo} alt="Logo" />
+        </div>
       </div>
     );
   }
